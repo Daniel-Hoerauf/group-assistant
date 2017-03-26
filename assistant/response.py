ca645c22ec307aab9371f8fe039d87ffa4ff2310
Torry Yang <torryyang@gmail.com>
2017-03-26 12:30:38 -0400

    test

--- assistant/response.py
+++ assistant/response.py
@@ -70,6 +70,7 @@ def math(expression):
         return 'That is some nasty math. Please try something else.'
 
 def news(topic):
+    return getenv('BING_SEARCH')
     headers = {
         # Request headers
         'Ocp-Apim-Subscription-Key': getenv('BING_SEARCH'),
