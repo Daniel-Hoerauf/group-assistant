858035d1826681b43ca3566d66b802bb28649466
Torry Yang <torryyang@gmail.com>
2017-03-26 12:44:00 -0400

    re-add weather

---
+++ assistant/weatherTest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+from weather import Weather
+
+def main(args):
+	weather = Weather()
+
+	location = weather.lookup_by_location(args[1])
+	condition = location.forecast()[0]
+
+	if condition:
+		return condition['text'] + ' with high of ' + condition['high'] + ' and low of ' + condition['low']
+	else:
+		return "City not found. It's probably raining meatballs. Please try again."
+
+if __name__ == '__main__':
+    main(sys.argv)
