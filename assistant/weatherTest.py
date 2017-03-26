b41ab090a0d777b1b7dd6a6f1e9076f43e6011cf
Torry Yang <torryyang@gmail.com>
2017-03-26 11:18:14 -0400

    debug

--- assistant/weatherTest.py
+++ assistant/weatherTest.py
@@ -3,15 +3,16 @@ import sys
 from weather import Weather
 
 def main(args):
-	weather = Weather()
+	print args
+	# weather = Weather()
 
-	location = weather.lookup_by_location(args[1])
-	condition = location.forecast()[0]
+	# location = weather.lookup_by_location(args[1])
+	# condition = location.forecast()[0]
 
-	if condition:
-		return condition['text'] + ' with high of ' + condition['high'] + ' and low of ' + condition['low']
-	else:
-		return "City not found. It's probably raining meatballs. Please try again."
-		
+	# if condition:
+	# 	return condition['text'] + ' with high of ' + condition['high'] + ' and low of ' + condition['low']
+	# else:
+	# 	return "City not found. It's probably raining meatballs. Please try again."
+
 if __name__ == '__main__':
     main(sys.argv)
